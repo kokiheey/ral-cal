6ec67a657d510bd9aa886c22a6130c4f8f5effa4
kpasic <kpasic22@rg.edu.rs>
2025-10-31 06:47:41 +0100

    Nuke project (again)

--- App.tsx
+++
@@ -1,9 +0,0 @@
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import  StopWatch from "./components/Stopwatch";
-export default function App() {
-  return (
-    <ScrollView>
-      <StopWatch />
-    </ScrollView>
-  );
-}
--- services/GoogleCalendarApi.ts
+++
@@ -1,7 +0,0 @@
-import { CalendarEvent } from "../types/CalendarEvent";
-import { getAccessToken } from './GoogleAuth';
-
-
-export async function createEvent (event: CalendarEvent, startTime: Date, endTime:Date){
-    const accessToken = await getAccessToken();
-}
