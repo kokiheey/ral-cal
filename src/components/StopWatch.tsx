6c39caad4e5483fbfc9f272cff77ba4d849738e6
svuskovic22 <tesla89karate@gmail.com>
2025-11-22 22:02:26 +0100

    Add pause

--- src/components/StopWatch.tsx
+++ src/components/StopWatch.tsx
@@ -8,51 +8,68 @@ interface StopWatchProps{
     onStop: () => void;
 }
 
-function StopWatch({onStart, onStop}:StopWatchProps) {
+function StopWatch({onStart, onStop}: StopWatchProps) {
+    const [isPaused, setIsPaused] = useState(false);
     const [isRunning, setIsRunning] = useState(false);
+    const [inSession, setInSession] = useState(false);
     const [elapsedTime, setElapsedTime] = useState(0);
-    const intervalIdRef = useRef<number>(0);
-    const startTimeRef = useRef<number>(0);
+    const uiStartRef = useRef(0); //za stopwatch
+    const segmentStartRef = useRef(0); //za "backend"
+    const intervalIdRef = useRef<NodeJS.Timer | null>(null);
 
-    async function handleStart(){
-        startTimeRef.current = Date.now();
-        //console.log(startTimeRef.current);
+    function handleStart(){
+        uiStartRef.current = Date.now();
+        segmentStartRef.current = Date.now();
+        saveStartTime(uiStartRef.current);
+        setElapsedTime(0);
         setIsRunning(true);
-        await saveStopWatchRunning(true);
-        await saveStartTime(startTimeRef.current);
+        setInSession(true);
+        onStart();
+    }
+
+    function handlePause(){
+        setIsPaused(true);
+        setIsRunning(false);
+        onStop();
+    }
+
+    function handleUnpause(){
+        setIsPaused(false);
+        setIsRunning(true);
+        uiStartRef.current = Date.now() - elapsedTime;
+        segmentStartRef.current = Date.now();
+        saveStartTime(uiStartRef.current);
         onStart();
     }
 
     function handleStop(){
         setIsRunning(false);
+        setInSession(false);
         saveStopWatchRunning(false);
         onStop();
-
         setElapsedTime(0);
     }
 
     useEffect(() => {
         if(isRunning){
             intervalIdRef.current = setInterval(() => {
-                setElapsedTime(Date.now() - startTimeRef.current);
+                setElapsedTime(Date.now() - uiStartRef.current);
             }, 10);
         }
-        return () => { 
-            clearInterval(intervalIdRef.current);
+        return () => {
+            if(intervalIdRef.current) clearInterval(intervalIdRef.current);
         }
     }, [isRunning]);
 
     useEffect(() =>{
         const loadInitialState = async () => {
-            
             try {
                 const stored = await loadStartTime();
                 if (stored !== null) {
                     const stopwatchState = await loadStopWatchRunning();
                     setIsRunning(stopwatchState || false);
-
                     if(stopwatchState){
-                        startTimeRef.current = stored;
+                        uiStartRef.current = stored;
                         setElapsedTime(Date.now() - stored);
                     } else {
                         setElapsedTime(0);
@@ -63,16 +80,16 @@ function StopWatch({onStart, onStop}:StopWatchProps) {
             }
         };
         loadInitialState();
-    },[])
+    },[]);
 
     function formatTime(){
-        let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
-        let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
-        let seconds = Math.floor(elapsedTime / (1000) % 60);
+        const hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+        const minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+        const seconds = Math.floor(elapsedTime / 1000 % 60);
 
-        let Shours = String(hours).padStart(2, "0");
-        let Sminutes = String(minutes).padStart(2,"0");
-        let Sseconds = String(seconds).padStart(2,"0");
+        const Shours = String(hours).padStart(2, "0");
+        const Sminutes = String(minutes).padStart(2,"0");
+        const Sseconds = String(seconds).padStart(2,"0");
         return `${Shours}:${Sminutes}:${Sseconds}`;
     }
 
@@ -86,23 +103,60 @@ function StopWatch({onStart, onStop}:StopWatchProps) {
                         </Svg>
             </View>
             <View className="flex-row mt-4">
-                <Pressable 
-                 className="flex-1 m-2 items-center justify-center active:bg-dark-200 bg-dark-100 border border-light-100 rounded-[25px] px-6 py-3"
-                 onPress={handleStart}
-                >
-                     {({ pressed }) => (
-                        <View
-                        className={
-                            pressed
-                            ? "opacity-60 scale-95"
-                            : "opacity-100 scale-100"
-                        }
-                        >
-                        <Text selectable={false} className="text-2xl text-light-100 font-bold select-none">Start</Text>
+                {inSession? (
+                    isPaused?(
+                        <Pressable
+                        className="flex-1 m-2 items-center justify-center active:bg-dark-200 bg-dark-100 border border-light-100 rounded-[25px] px-6 py-3"
+                        onPress={handleUnpause}>
+                            {({ pressed }) => (
+                                <View
+                                className={
+                                    pressed
+                                    ? "opacity-60 scale-95"
+                                    : "opacity-100 scale-100"
+                                }
+                                >
+                                <Text selectable={false} className="text-2xl text-light-100 font-bold select-none">Unpause</Text>
 
-                        </View>
-                     )}
+                                </View>
+                            )}
+                        </Pressable>
+                    ):(
+                        <Pressable
+                        className="flex-1 m-2 items-center justify-center active:bg-dark-200 bg-dark-100 border border-light-100 rounded-[25px] px-6 py-3"
+                        onPress={handlePause}>
+                            {({ pressed }) => (
+                                <View
+                                className={
+                                    pressed
+                                    ? "opacity-60 scale-95"
+                                    : "opacity-100 scale-100"
+                                }
+                                >
+                                <Text selectable={false} className="text-2xl text-light-100 font-bold select-none">Pause</Text>
+
+                                </View>
+                            )}
+                        </Pressable>
+                    )
+                ):(
+                    <Pressable
+                    className="flex-1 m-2 items-center justify-center active:bg-dark-200 bg-dark-100 border border-light-100 rounded-[25px] px-6 py-3"
+                    onPress={handleStart}>
+                        {({ pressed }) => (
+                            <View
+                            className={
+                                pressed
+                                ? "opacity-60 scale-95"
+                                : "opacity-100 scale-100"
+                            }
+                            >
+                            <Text selectable={false} className="text-2xl text-light-100 font-bold select-none">Start</Text>
+
+                            </View>
+                        )}
                     </Pressable>
+                )}
                 <Pressable className="flex-1 m-2 items-center justify-center active:bg-dark-200 bg-dark-100 border border-light-100 rounded-[25px] px-6 py-3" onPress={handleStop}>
                     {({ pressed }) => (
                         <View
